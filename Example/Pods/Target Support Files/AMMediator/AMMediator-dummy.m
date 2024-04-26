7682a583d05a6682d4a5cea42bb36cc5ad37a5d5
wei.hu <7430328@qq.com>
2024-04-26 10:19:08 +0800

    upload
    upload

---
+++ Example/Pods/Target Support Files/AMMediator/AMMediator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AMMediator : NSObject
+@end
+@implementation PodsDummy_AMMediator
+@end
---
+++ Example/Pods/Target Support Files/Pods-AMMediator_Tests/Pods-AMMediator_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AMMediator_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AMMediator_Tests
+@end
